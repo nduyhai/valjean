604c274bc13198e64a719295b3eac8bd4ace5a53
nduyhai <nduyhai@users.noreply.github.com>
2025-10-06 20:28:08 +0700

    chore: refine code

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,14 @@
 # Dockerfile
-ARG APP_NAME=Vajean
 FROM golang:1.25 AS builder
-
 WORKDIR /app
-
 COPY . .
-
-ARG APP_NAME=Vajean
-ENV APP_NAME=vajean
-
 RUN go mod download
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o vajean .
 
-FROM alpine
-
+FROM alpine:3.22.1
 WORKDIR /app
-
-ARG APP_NAME=Vajean
-ENV APP_NAME=vajean
-
-COPY --from=builder /app/vajean .
-
-# Fix permissions
+COPY --from=builder /app/vajean ./vajean
 RUN chmod +x vajean
 
 EXPOSE 8080
-ENTRYPOINT ["sh", "-c", "./vajean"]
+ENTRYPOINT ["./vajean"]
--- internal/app/usecase/evaluate.go
+++ internal/app/usecase/evaluate.go
@@ -15,31 +15,31 @@ import (
 )
 
 type EvaluateUseCase struct {
-	Evaluator     ports.Evaluator
-	Moderation    service.Moderation
-	RateLimiter   ports.RateLimiter
-	EventProducer ports.EventProducer
-	Telegram      config.Telegram
-	Cooldown      time.Duration
-	Logger        *slog.Logger
+	evaluator     ports.Evaluator
+	moderation    service.Moderation
+	rateLimiter   ports.RateLimiter
+	eventProducer ports.EventProducer
+	telegram      config.Telegram
+	cooldown      time.Duration
+	logger        *slog.Logger
 }
 
 func NewEvaluateUseCase(evaluator ports.Evaluator, moderation service.Moderation, rateLimiter ports.RateLimiter, eventProducer ports.EventProducer, config config.Config, logger *slog.Logger) *EvaluateUseCase {
-	return &EvaluateUseCase{Evaluator: evaluator, Moderation: moderation, RateLimiter: rateLimiter, EventProducer: eventProducer, Telegram: config.Telegram, Cooldown: 2 * time.Second, Logger: logger}
+	return &EvaluateUseCase{evaluator: evaluator, moderation: moderation, rateLimiter: rateLimiter, eventProducer: eventProducer, telegram: config.Telegram, cooldown: 2 * time.Second, logger: logger}
 
 }
 
 func (e *EvaluateUseCase) ShouldHandle(ctx context.Context, in entities.EvalInput) bool {
 	text := strings.TrimSpace(in.Text)
-	if e.Telegram.Prefix != "" && strings.HasPrefix(text, e.Telegram.Prefix) {
+	if e.telegram.Prefix != "" && strings.HasPrefix(text, e.telegram.Prefix) {
 		return true
 	}
-	if e.Telegram.BotUsername != "" &&
-		strings.Contains(text, "@"+e.Telegram.BotUsername) {
+	if e.telegram.BotUsername != "" &&
+		strings.Contains(text, "@"+e.telegram.BotUsername) {
 		return true
 	}
 
-	if in.ReplyFor == e.Telegram.BotUsername {
+	if in.ReplyFor == e.telegram.BotUsername {
 		return true
 	}
 
@@ -52,26 +52,26 @@ func (e *EvaluateUseCase) ShouldHandle(ctx context.Context, in entities.EvalInpu
 
 func (e *EvaluateUseCase) Handle(ctx context.Context, in entities.EvalInput) error {
 	// rate limit
-	ok, _ := e.RateLimiter.Allow(ctx, rlKey(in), 1)
+	ok, _ := e.rateLimiter.Allow(ctx, rlKey(in), 1)
 	if !ok {
-		e.Logger.Warn("rate limit exceeded")
+		e.logger.Warn("rate limit exceeded")
 		return errors.New("cooling down—try again in a moment")
 	}
 	// moderation
-	allowed, reason := e.Moderation.Allowed(ctx, in.Text)
+	allowed, reason := e.moderation.Allowed(ctx, in.Text)
 	if !allowed {
-		e.Logger.Warn("message skipped")
+		e.logger.Warn("message skipped")
 		return errors.New("Message skipped (" + reason + ")")
 	}
 	// normalize text (trim trigger)
-	in.Text = stripTriggers(in.Text, e.Telegram)
-	out, err := e.Evaluator.Evaluate(ctx, in)
+	in.Text = stripTriggers(in.Text, e.telegram)
+	out, err := e.evaluator.Evaluate(ctx, in)
 	if err != nil || out.Summary == "" {
-		e.Logger.Error("failed to evaluate", slog.Any("error", err))
+		e.logger.Error("failed to evaluate", slog.Any("error", err))
 		return errors.New("i couldn’t evaluate that right now")
 	}
 
-	e.EventProducer.Publish(ctx, entities.Event{
+	e.eventProducer.Publish(ctx, entities.Event{
 		ChatID:            in.ChatID,
 		OriginalMessageId: in.MessageID,
 		ReplyMessage:      out.Summary,
