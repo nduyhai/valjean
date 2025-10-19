3a5bcd7cfcceb7db659655dc260086ef8311e2b5
nduyhai <nduyhai@users.noreply.github.com>
2025-10-19 10:07:48 +0700

    chore: always allow zl

--- internal/app/service/moderation_zalo.go
+++ internal/app/service/moderation_zalo.go
@@ -2,8 +2,6 @@ package service
 
 import (
 	"context"
-	"slices"
-	"strings"
 
 	"github.com/nduyhai/valjean/internal/app/entities"
 	"github.com/nduyhai/valjean/internal/infra/config"
@@ -17,29 +15,6 @@ func newZaloModeration(config config.Zalo) moderationStrategy {
 	return &zaloModeration{config: config}
 }
 
-func (m *zaloModeration) Allowed(_ context.Context, in entities.EvalInput) bool {
-	text := strings.TrimSpace(in.Text)
-
-	if len(m.config.BlockedUsers) > 0 && slices.Contains(m.config.BlockedUsers, in.UserHandle) {
-		return false
-	}
-
-	if len(m.config.AllowedUsers) > 0 && !slices.Contains(m.config.AllowedUsers, in.UserHandle) {
-		return false
-	}
-
-	if m.config.BotUsername != "" &&
-		strings.Contains(text, "@"+m.config.BotUsername) {
-		return true
-	}
-
-	if in.ReplyFor == m.config.BotUsername {
-		return true
-	}
-
-	if in.ChatType == PrivateChat {
-		return true
-	}
-
-	return false
+func (m *zaloModeration) Allowed(_ context.Context, _ entities.EvalInput) bool {
+	return true
 }
